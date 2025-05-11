827acf330ed399dbec4e603cf0ec39e0313e9dce
Polina Bodnar <153727053+PolinaBodnar@users.noreply.github.com>
2025-05-11 21:29:56 +0200

    Update Transport.java

--- src/transport/Transport.java
+++ src/transport/Transport.java
@@ -1,18 +1,16 @@
 package transport;
 
 public abstract class Transport {
-    public int speed;  // приватное поле
+    public int speed;  
 
     public Transport(int speed) {
         this.speed = speed;
     }
 
-    // Геттер для поля speed
     public int getSpeed() {
         return speed;
     }
 
-    // Сеттер для поля speed (если нужно изменять его позже)
     public void setSpeed(int speed) {
         this.speed = speed;
     }
